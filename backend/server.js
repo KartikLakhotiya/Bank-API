b3edf0f6223ee11f0812de4476ed4cf67c0147f5
Kartik Lakhotiya <kartik.lakhotiya064@nmims.in>
2026-02-01 16:41:36 +0530

    typo

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:5173","https://bank-api-tan.vercel.app/"],
-    credentials: true
+    origin: ["http://localhost:5173", "https://bank-api-tan.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 logger.info('Server', 'Middlewares initialized successfully');

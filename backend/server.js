23b1def70a6c515f72fb02a9771bb131308b4a0f
Kartik Lakhotiya <kartik.lakhotiya064@nmims.in>
2026-02-01 16:40:03 +0530

    added frotend prod URL in CORS

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://bank-api-tan.vercel.app/"],
     credentials: true
 }))
 
